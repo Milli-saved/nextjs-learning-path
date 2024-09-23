b60c4e47fafebc0a2c4e6709de0e887be4cd3960
Milli-saved <million12.tenkir@gmail.com>
2024-09-23 14:11:50 +0300

    fix: fix the home page.

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,20 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover and Share</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI powered prompts</span>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nobis
+        aliquam cum eius corporis iusto, sequi ex minus ipsa. Dolorum, nulla sed
+        quo molestiae quod quas libero ullam explicabo veniam.
+      </p>
+      {/* Feed */}
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
