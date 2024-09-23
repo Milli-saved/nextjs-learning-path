d549cb4bc43d9d2ae85cd5c26b7a1971ae49fed5
Milli-saved <million12.tenkir@gmail.com>
2024-09-23 15:16:06 +0300

    start on the google authentication.

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,6 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
-
-
 export const metadata = {
   title: "Promptopia",
   description: "Discover and share AI prompts",
@@ -14,13 +12,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
